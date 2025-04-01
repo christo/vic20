add70269cbb86f160ce1c0893583c566eea3c805
Chris Mountford <chris.mountford@gmail.com>
2025-04-01 20:37:22 +1100

    hello world program with basic stub

---
+++ tce/tce.asm
@@ -0,0 +1,22 @@
+    * = $1001 "basic stub then machine code"
+    .word nextLine
+    .word 2025
+    .byte $9e
+    .text "4110"
+    .byte 0
+nextLine:
+    .byte 0,0,0
+entry:
+    ldx #0
+loop:
+    lda hello, x
+    beq end
+    jsr $ffd2
+    inx
+    jmp loop
+end:
+    rts
+
+hello:
+    .text "HELLO WORLD"
+    .byte 0
